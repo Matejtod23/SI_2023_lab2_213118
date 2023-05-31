c6a6a643070be722598b1acf95ec7f0f9bb1603e
Matej <matej.todorovsi1@gmail.com>
2023-05-31 02:21:36 +0200

    last changes

--- src/test/java/SILab2Test.java
+++ src/test/java/SILab2Test.java
@@ -10,12 +10,8 @@ class SILab2Test {
 
     private SILab2 function = new SILab2();
 
-    @Test
-    public void testValidUser() {
-        User user = new User("TestOne", "password123@", "testone@gmail.com");
-        List<User> allUsers = new ArrayList<>();
-        Assertions.assertTrue(function.function(user, allUsers));
-    }
+
+
     @Test
     public void testNullUser() {
         List<User> allUsers = new ArrayList<>();
@@ -23,59 +19,23 @@ class SILab2Test {
     }
 
     @Test
-    public void testMissingPassword() {
-        User user = new User("TestOne", null, "testone@gmail.com");
-        List<User> allUsers = new ArrayList<>();
-        Assertions.assertThrows(RuntimeException.class, () -> function.function(user, allUsers));
-    }
-
-    @Test
-    public void testMissingEmail() {
-        User user = new User("TestOne", "password123", null);
-        List<User> allUsers = new ArrayList<>();
-        Assertions.assertThrows(RuntimeException.class, () -> function.function(user, allUsers));
-    }
-
-    @Test
-    public void testMissingMandatoryInformation() {
-        User user = new User("TestOne", null, null);
-        List<User> allUsers = new ArrayList<>();
-        Assertions.assertThrows(RuntimeException.class, () -> function.function(user, allUsers));
-    }
-
-    @Test
-    public void testNullUsername() {
-        User user = new User(null, "password123", "testone@gmail.com");
+    public void testInvalidEmail() {
+        User user = new User("TestOne", "password123", "invalidemail");
         List<User> allUsers = new ArrayList<>();
         Assertions.assertFalse(function.function(user, allUsers));
-        Assertions.assertEquals(user.getEmail(), user.getUsername());
     }
 
     @Test
-    public void testExistingUser() {
-        User user = new User("TestOne", "password123", "testone@gmail.com");
+    public void testEmailOrUsernameAlreadyTaken() {
+        User user = new User("TestOne", "password", "existingemail@example.com");
         List<User> allUsers = new ArrayList<>();
-        allUsers.add(new User("existing", "existing123", "existing123@gmail.com"));
+        allUsers.add(new User("other", "password", "existingemail@example.com"));
         Assertions.assertFalse(function.function(user, allUsers));
     }
 
     @Test
     public void testPasswordContainsUsername() {
         User user = new User("testone", "testone123", "testone@gmail.com");
-        List<User> allUsers = new ArrayList<>();
-        Assertions.assertFalse(function.function(user, allUsers));
-    }
-
-    @Test
-    public void testShortPassword() {
-        User user = new User("TestOne", "pass", "testone@gmail.com");
-        List<User> allUsers = new ArrayList<>();
-        Assertions.assertFalse(function.function(user, allUsers));
-    }
-
-    @Test
-    public void testPasswordWithoutSpecialCharacters() {
-        User user = new User("TestOne", "password123", "testone@gmail.com");
         List<User> allUsers = new ArrayList<>();
         Assertions.assertFalse(function.function(user, allUsers));
     }
